1a396ae8155fdfb35fe0212e77dde39f118f1647
Monica Wojciechowska <monica.wojciechowska@netguru.pl>
2019-12-11 16:47:31 +0100

    Updates

---
+++ precache-manifest.e2cc4e5c64a01a26353f954e0b0738a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c99228821b0229e094764418a190126",
+    "url": "/shaping-the-path/index.html"
+  },
+  {
+    "revision": "67bf0c819307c4e06749",
+    "url": "/shaping-the-path/static/css/main.14b3489c.chunk.css"
+  },
+  {
+    "revision": "c8758675f953a2252894",
+    "url": "/shaping-the-path/static/js/2.607746a0.chunk.js"
+  },
+  {
+    "revision": "5d83b32028c822b364148904e89bb0e7",
+    "url": "/shaping-the-path/static/js/2.607746a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67bf0c819307c4e06749",
+    "url": "/shaping-the-path/static/js/main.ae8c8df6.chunk.js"
+  },
+  {
+    "revision": "8a806975a6d0522917bf",
+    "url": "/shaping-the-path/static/js/runtime-main.5177cecb.js"
+  },
+  {
+    "revision": "28abba4152d48e57f4b189d5a32dfc02",
+    "url": "/shaping-the-path/static/media/white-paper-background.28abba41.jpg"
+  }
+]);
